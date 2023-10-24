c7c5e6addb7b92c73ac258af6900c76f4b72384c
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-10-24 23:17:29 +0100

    add tests to fund function

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "chai"
+import { assert, expect } from "chai"
 import { FundMe, MockV3Aggregator } from "../../typechain-types"
 import { deployments, ethers } from "hardhat"
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers"
@@ -6,6 +6,7 @@ describe("FundMe", async () => {
     let fundMe: FundMe
     let deployer: SignerWithAddress
     let mockV3Aggregator: MockV3Aggregator
+    const sendValue = ethers.parseEther("1") //1 ETH
     beforeEach(async () => {
         // deploy our fund me contract
         // using hardhat-deploy
@@ -30,5 +31,22 @@ describe("FundMe", async () => {
             const response = await fundMe.s_priceFeed()
             assert.equal(response, await mockV3Aggregator.getAddress())
         })
+    })
+
+    describe("fund", async () => {
+        it("fails if you dont sent enough ETH", async () => {
+            await expect(fundMe.fund()).to.be.revertedWith("You need to spend more ETH!")
+        })
+        it("updates the amount funded data structure", async () => {
+            await fundMe.fund({ value: sendValue })
+            const resp = await fundMe.s_addressToAmountFunded(deployer.address)
+            assert.equal(resp.toString(), sendValue.toString())
+        })
+        it("adds funder to array of funders", async () => {
+            await fundMe.fund({ value: sendValue })
+            const funder = await fundMe.s_funders(0)
+            assert.equal(funder, deployer.address)
+        })
+
     })
 })
