fd2856375e7c04bc20cfd6b3d1de4ecb994409ce
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-10-26 19:32:04 +0100

    add scripts for fund and withdraw

---
+++ scripts/withdraw.ts
@@ -0,0 +1,17 @@
+import { ethers, getNamedAccounts } from "hardhat"
+
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContractAt("FundMe", deployer)
+    console.log("Funding Contract...")
+    const txResponse = await fundMe.withdraw()
+    await txResponse.wait(1)
+    console.log("Got it back!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
